949ec646c79673dd4a4adb21a6eb10ae4d199ba7
ArnoSchiller <arno-schiller@hotmail.de>
2023-07-11 07:22:50 +0200

    Included missing rotation check

--- greedypacker/maximal_rectangles.py
+++ greedypacker/maximal_rectangles.py
@@ -14,6 +14,7 @@ from .item import Item
 
 class FreeRectangle(typing.NamedTuple('FreeRectangle', [('width', int), ('height', int), ('x', int), ('y', int)])):
     __slots__ = ()
+
     @property
     def area(self):
         return self.width*self.height
@@ -49,35 +50,33 @@ class MaximalRectangle:
             raise ValueError('No such heuristic!')
 
         if x == 0 or y == 0:
-            self.freerects = [] # type: List[FreeRectangle]
+            self.freerects = []  # type: List[FreeRectangle]
         else:
-            self.freerects = [FreeRectangle(self.x, self.y, 0, 0)] # type: List[FreeRectangle]
-        self.items = [] # type: List[Item]
+            # type: List[FreeRectangle]
+            self.freerects = [FreeRectangle(self.x, self.y, 0, 0)]
+        self.items = []  # type: List[Item]
         self.rotation = rotation
-
 
     def __repr__(self) -> str:
         return "MaximalRectangle(%r)" % (self.items)
 
-
     @staticmethod
     def _item_fits_rect(item: Item,
-                       rect: FreeRectangle,
-                       rotation: bool=False) -> bool:
+                        rect: FreeRectangle,
+                        rotation: bool = False) -> bool:
         if (not rotation and
-            item.width <= rect.width and 
-            item.height <= rect.height):
+            item.width <= rect.width and
+                item.height <= rect.height):
             return True
-        if (rotation and 
-            item.height <= rect.width and 
-            item.width <= rect.height):
+        if (rotation and
+            item.height <= rect.width and
+                item.width <= rect.height):
             return True
         return False
 
-
     @staticmethod
     def _split_rectangle(rectangle: FreeRectangle,
-                        item: Item) -> List[FreeRectangle]:
+                         item: Item) -> List[FreeRectangle]:
         """
         Return a list of maximal free rectangles from a split
         """
@@ -94,14 +93,13 @@ class MaximalRectangle:
         if item.height < rectangle.height:
             Fw = rectangle.width
             Fh = rectangle.height - item.height
-            Fx = rectangle.x 
+            Fx = rectangle.x
             Fy = rectangle.y + item.height
             results.append(FreeRectangle(width=Fw,
                                          height=Fh,
                                          x=Fx,
                                          y=Fy))
         return results
-    
 
     @staticmethod
     def _item_bounds(item: Item) -> tuple:
@@ -114,10 +112,9 @@ class MaximalRectangle:
                 item.x+item.width,
                 item.y+item.height)
 
-
     @staticmethod
     def _check_intersection(free_rect: FreeRectangle,
-                           bounding_box: tuple) -> bool:
+                            bounding_box: tuple) -> bool:
         """ 
         Checks if bounding box intersects rectangle
         """
@@ -126,13 +123,12 @@ class MaximalRectangle:
         if (bounding_box[0] >= free_rect.x + free_rect.width or
             bounding_box[2] <= free_rect.x or
             bounding_box[1] >= free_rect.y + free_rect.height or
-            bounding_box[3] <= free_rect.y):
+                bounding_box[3] <= free_rect.y):
             return False
         return True
-        
 
     @staticmethod
-    def _find_overlap(F1: FreeRectangle, F2: tuple ) -> tuple:
+    def _find_overlap(F1: FreeRectangle, F2: tuple) -> tuple:
         """
         returns the bottom left and top right 
         coordinates of the overlap
@@ -151,7 +147,7 @@ class MaximalRectangle:
 
     @staticmethod
     def _clip_overlap(rect: FreeRectangle,
-                         overlap: tuple) -> List[FreeRectangle]:
+                      overlap: tuple) -> List[FreeRectangle]:
         """
         Return maximal rectangles for  non-intersected 
         parts of rect.
@@ -159,10 +155,10 @@ class MaximalRectangle:
         Fx, Fy = rect.x, rect.y
         Fw, Fh = rect.width, rect.height
         Ox1, Oy1, Ox2, Oy2 = overlap
-                
+
         results = []
-        
-        ## Check for non-intersected sections
+
+        # Check for non-intersected sections
         # Left Side
         if Ox1 > Fx:
             L = FreeRectangle(width=Ox1-Fx, height=Fh, x=Fx, y=Fy)
@@ -181,11 +177,10 @@ class MaximalRectangle:
             results.append(T)
 
         return results
-       
-        
+
     @staticmethod
     def _encapsulates(F0: FreeRectangle,
-                     F1: FreeRectangle) -> bool:
+                      F1: FreeRectangle) -> bool:
         """
         Returns true if F1 is fully encapsulated
         inside F0
@@ -199,7 +194,6 @@ class MaximalRectangle:
         if F1.y+F1.height > F0.y+F0.height:
             return False
         return True
-
 
     def _remove_redundent(self) -> List[FreeRectangle]:
         """
@@ -221,13 +215,12 @@ class MaximalRectangle:
             i += 1
         return self.freerects
 
-
     def _prune_overlaps(self, itemBounds: tuple) -> None:
         """
         Loop through all FreeRectangles and prune
         any overlapping the itemBounds
         """
-        result = [] # type: List[FreeRectangle]
+        result = []  # type: List[FreeRectangle]
         for rect in self.freerects:
             if self._check_intersection(rect, itemBounds):
                 overlap = self._find_overlap(rect, itemBounds)
@@ -238,20 +231,18 @@ class MaximalRectangle:
         self.freerects = result
         self._remove_redundent()
 
-    
     def _find_best_score(self, item: Item):
         rects = []
         for rect in self.freerects:
             if self._item_fits_rect(item, rect):
                 rects.append((self._score(rect, item, self), rect, False))
-            if self._item_fits_rect(item, rect, rotation=True):
+            if self.rotation and self._item_fits_rect(item, rect, rotation=True):
                 rects.append((self._score(rect, item, self), rect, True))
         try:
             _score, rect, rot = min(rects, key=lambda x: x[0])
             return _score, rect, rot
         except ValueError:
             return None, None, False
-
 
     def insert(self, item: Item, heuristic: str = 'best_area') -> bool:
         """
@@ -274,7 +265,6 @@ class MaximalRectangle:
             return True
         return False
 
-
     def bin_stats(self) -> dict:
         """
         Returns a dictionary with compiled stats on the bin tree
@@ -286,7 +276,7 @@ class MaximalRectangle:
             'area': self.area,
             'efficiency': (self.area - self.free_area) / self.area,
             'items': self.items,
-            }
+        }
 
         return stats
 
@@ -294,7 +284,7 @@ class MaximalRectangle:
 def scoreBAF(rect: FreeRectangle, item: Item, self) -> int:
     """ Best Area Fit """
     return rect.area-item.area, min(rect.width-item.width, rect.height-item.height)
-        
+
 
 def scoreBSSF(rect: FreeRectangle, item: Item, self) -> int:
     """ Best Short Side Fit """
@@ -309,7 +299,7 @@ def scoreBLSF(rect: FreeRectangle, item: Item, self) -> int:
 def scoreWAF(rect: FreeRectangle, item: Item, self) -> int:
     """ Worst Area Fit """
     return (0 - (rect.area-item.area)), (0 - min(rect.width-item.width, rect.height-item.height))
-        
+
 
 def scoreWSSF(rect: FreeRectangle, item: Item, self) -> int:
     """ Worst Short Side Fit """
@@ -336,6 +326,7 @@ def common_interval_length(Xstart: int, Xend: int,
         return 0
     return min(Xend, Yend) - max(Xstart, Ystart)
 
+
 def scoreCP(rect: FreeRectangle, item: Item, self) -> int:
     """ Contact Point """
     perim = 0
@@ -346,9 +337,11 @@ def scoreCP(rect: FreeRectangle, item: Item, self) -> int:
 
     for itm in self.items:
         if (itm.x == rect.x+rect.width or
-            itm.x+itm.width == rect.x):
-            perim += common_interval_length(itm.y, itm.y+itm.height, rect.y, rect.y+item.height)
+                itm.x+itm.width == rect.x):
+            perim += common_interval_length(itm.y, itm.y +
+                                            itm.height, rect.y, rect.y+item.height)
         if (itm.y == rect.y+rect.height or
-            item.y+itm.height == rect.y):
-            perim += common_interval_length(itm.x, itm.x+itm.width, rect.x, rect.x+item.width)
+                item.y+itm.height == rect.y):
+            perim += common_interval_length(itm.x,
+                                            itm.x+itm.width, rect.x, rect.x+item.width)
     return (0 - perim), min(rect.width-item.width, rect.height-item.height)
